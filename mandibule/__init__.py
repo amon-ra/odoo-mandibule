210d8a7d05a2061f96130244762f9f63978498e3
seb <seb@localhost>
2013-05-22 18:38:25 +0200

    Initial commit

---
+++ mandibule/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+##############################################################################
+#
+#    Mandibule, an explorer for OpenERP servers
+#    Copyright (C) 2013  Sébastien Alix
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import sys
+
+from PySide.QtGui import QApplication, QLabel
+
+app = QApplication(sys.argv)
+
+label = QLabel("Mandibule!")
+label.show()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
