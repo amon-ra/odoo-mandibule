9fbbd31fecc573fb470fc1b10563b68d214f3b65
seb <seb@localhost>
2013-11-13 18:14:04 +0100

    New 'mandibule.controlers' package containing the 'GroupControler' class + added 'mandibule.db' module to manage data produced by the application.

---
+++ mandibule/controlers/group.py
@@ -0,0 +1,85 @@
+# -*- coding: UTF-8 -*-
+##############################################################################
+#
+#    Mandibule, an explorer for OpenERP servers
+#    Copyright (C) 2013 Sébastien Alix
+#                       Frédéric Fidon
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import uuid
+
+from PySide.QtCore import QObject, Signal
+
+from mandibule import db
+from mandibule.utils.i18n import _
+from mandibule.utils.form import FormDialog, TextField
+
+
+class GroupControler(QObject):
+    """Group controler."""
+    created = Signal(str)
+    updated = Signal(str)
+    deleted = Signal(str)
+
+    def display_form(self, id_=None):
+        """Display the form to create/update a group."""
+        data = self.read(id_) or {}
+        fields = [
+            ('name', TextField(_("Name"), data.get('name', ''))),
+        ]
+        new_data, ok = FormDialog(fields).exec_()
+        if ok:
+            if id_:
+                self.update(id_, new_data)
+            else:
+                self.create(new_data)
+        return ok
+
+    def create(self, data):
+        """Create a new group and return its ID."""
+        id_ = uuid.uuid4().hex
+        data['servers'] = []
+        db_data = db.read()
+        db_data[id_] = data
+        db.write(db_data)
+        self.created.emit(id_)
+        return id_
+
+    def read(self, id_):
+        """Return data related to a group."""
+        db_data = db.read()
+        if id_ in db_data:
+            # We remove servers from data returned
+            del db_data[id_]['servers']
+            return db_data[id_]
+        return None
+
+    def update(self, id_, data):
+        """Update a group."""
+        db_data = db.read()
+        if id_ in db_data:
+            db_data[id_].update(data)
+            db.write(db_data)
+            self.updated.emit(id_)
+
+    def delete(self, id_):
+        """Delete a group."""
+        db_data = db.read()
+        if id_ in db_data:
+            del db_data[id_]
+            db.write(db_data)
+            self.deleted.emit(id_)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ mandibule/db.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+##############################################################################
+#
+#    Mandibule, an explorer for OpenERP servers
+#    Copyright (C) 2013 Sébastien Alix
+#                       Frédéric Fidon
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import os.path
+import json
+
+from mandibule.utils import env
+
+
+CONFIG_PATH = env.save_user_config_path('mandibule')
+DATA_FILE = os.path.join(CONFIG_PATH, 'data.json')
+
+
+def read():
+    """Read data from DATA_FILE."""
+    with open(DATA_FILE, 'r') as fp:
+        return json.load(fp)
+
+
+def write(data):
+    """Write data in DATA_FILE."""
+    with open(DATA_FILE, 'w') as fp:
+        json.dump(data, fp, indent=4)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
