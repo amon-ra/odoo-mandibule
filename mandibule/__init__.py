4698815e47c61d1e2acbab775b0938b414bf17f8
seb <seb@localhost>
2013-11-29 17:37:06 +0100

    'setup.py' and 'mandibule/__init__.py' files updated

--- mandibule/__init__.py
+++ mandibule/__init__.py
@@ -18,6 +18,10 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+__author__ = 'Sebastien Alix'
+__email__ = 'seb@usr-src.org'
+__licence__ = 'GPL v3'
+__version__ = '0.1.0'
 
 from PySide import QtGui, QtCore
 
--- setup.py
+++ setup.py
@@ -4,36 +4,14 @@ import os
 from distutils.core import setup
 
 name = 'Mandibule'
-version = '0.1'
+version = '0.1.0'
 description = "Mandibule is a graphical tool to explore OpenERP servers."
 keywords = "openerp client developper analyse data metadata model oerplib"
 author = u"Sebastien Alix"
 author_email = 'seb@usr-src.org'
-#url = 'https://bitbucket.org/Mandibule/'
-#download_url = 'https://bitbucket.org/Mandibule/Mandibule-%s.tar.gz' % version
+#url = 'https://bitbucket.org/mandibule/'
+#download_url = 'https://bitbucket.org/mandibule/Mandibule-%s.tar.gz' % version
 license = 'GPLv3'
-doc_build_dir = 'doc/build'
-doc_source_dir = 'doc/source'
-
-cmdclass = {}
-command_options = {}
-# 'build_doc' option
-try:
-    from sphinx.setup_command import BuildDoc
-    if not os.path.exists(doc_build_dir):
-        os.mkdir(doc_build_dir)
-    cmdclass['build_doc'] = BuildDoc
-    command_options['build_doc'] = {
-        #'project': ('setup.py', name),
-        'version': ('setup.py', version),
-        'release': ('setup.py', version),
-        'source_dir': ('setup.py', doc_source_dir),
-        'build_dir': ('setup.py', doc_build_dir),
-        'builder': ('setup.py', 'html'),
-    }
-except Exception:
-    print("No Sphinx module found. You have to install Sphinx "
-          "to be able to generate the documentation.")
 
 setup(
     name=name,
@@ -54,15 +32,12 @@ setup(
         'mandibule.views.widgets',
         'mandibule.views.workarea',
     ],
-    include_package_data=True,
-    install_requires=[
-        'OERPLib',
-        'PySide',
+    requires=[
+        'OERPLib (>=0.8)',
+        'PySide (>=1.1)',
     ],
     scripts=['bin/mandibule'],
     license=license,
-    cmdclass=cmdclass,
-    command_options=command_options,
     classifiers=[
         "Environment :: X11 Applications :: Qt",
         "Intended Audience :: Developers",
